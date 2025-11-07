aaef9da8ef1c1efc908120b8ea53bee42899469f
Marie Johansen <majoh@itu.dk>
2025-11-07 08:14:22 +0100

    One step closer
    Co-authored-by: Charlotte Planteig <cpla@itu.dk>
    Co-authored-by: Sara Bagger <salb@itu.dk>

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # HW4
 This repository is for Distributed System Homework 4
+
+How to:
+# Terminal 1
+go run . --ID=1 --Port=:5050 --OtherPorts=:5051,:5052
+# Terminal 2
+go run . --ID=2 --Port=:5051 --OtherPorts=:5050,:5052
+# Terminal 3
+go run . --ID=3 --Port=:5052 --OtherPorts=:5050,:5051
--- nodes/node.go
+++ nodes/node.go
@@ -1,60 +1,132 @@
-package nodes
+package main
 
 import (
 	proto "ITUServer/grpc" //make connection
-	"bufio"
 	"context" //make connection - the context of the connection
-	"errors"  //create custom errors
-	"fmt"
+	"flag"    //command-line handling
+	"strings"
+
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
 	"io"
 	"log" //logs - used to keep track of messages
 	"net" //make connection to net
 	"os"
-	"unicode/utf8" //used to verify number of chars
 )
 
 type NodeServer struct {
-	proto.UnimplementedNodeServer //part of the proto - we are creating an Unimplemented server
-	lamportClock                  int32
-	logFile                       *os.File
+	proto.UnimplementedRicartArgawalaServer //part of the proto - we are creating an Unimplemented server
+	logFile                                 *os.File
 }
 
 type Node struct {
-	NodeId int32
-	State  string
+	Node           proto.RicartArgawalaClient
+	NodeId         int32
+	Lamport        int32
+	LamportRequest int32
+	State          string
+	Queue          []*proto.Client
+	OtherNodes   map[int32]proto.RicartArgawalaClient
+}
+
+type Config struct { //configuration for command-line setup, used for id and port-handling
+	ID         int32
+	Port       string
+	OtherPorts []string //the other clients ports comma-seperated //todo: rename?
+}
+
+// flag for setting up a new id, ports etc. Part of Config struct
+func configurationSetup() Config {
+	id := flag.Int("ID", 1, "node ID")
+	port := flag.String("Port", ":5050", "listen address, e.g. :5050")
+	otherPorts := flag.String("OtherPorts", "", "comma-seperated hosts, made of port of others")
+	flag.Parse()
+
+	var otherPortsHosts []string
+	if *otherPorts != "" {
+		otherPortsHosts = strings.Split(*otherPorts, ",")
+	}
+	return Config{
+		ID:         int32(*id),
+		Port:       *port,
+		OtherPorts: otherPortsHosts}
 }
 
 func main() {
+	configuration := configurationSetup()
+	//setup server part
 	server := &NodeServer{}
-	//creation of clients here
-	server.start_server()
+	server.start_server(configuration.Port)
+	log.Printf("DEBUG - ID=%d Port=%s", configuration.ID, configuration.Port) //todo delete debug
+
+	//setup the client part //todo: do for each port in 'OtherPorts' in the config
+	clientAddress := "localhost" + configuration.Port
+	conn, err := grpc.NewClient(clientAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Not working")
+	}
+
+	client := proto.NewRicartArgawalaClient(conn)
+	//todo: save 'client' in a list with clients for 'OtherPorts'
+
+	n := Node{
+		Node: client,
+		NodeId: configuration.ID//TODO: set node id, how? given in the commandline? yes - doneee
+		Lamport: 0,
+		State:   "RELEASED",
+	}
+
+	go start_client(n)
+
 }
 
-func (s *NodeServer) start_server() {
-	//local event: server start
-	s.lamportClock++
+func start_client(n Node) {
+	if n.State == "RELEASED" {
+		n.Lamport++
+		n.State = "WANTED"
+		//todo: call EnterRequest for all other clients (configured from 'OtherPorts')
+		//todo: spawn go routines for each request and wait for answer
+		response, err := n.Node.EnterRequest(context.Background(), n.getClientInfo())
+		if err != nil {
+			log.Fatalf("did not recieve anything or failed to send %v", err)
+		}
+		//go routine der lytter svar på alle
+	}
+}
 
-	grpcServer := grpc.NewServer()          //creates new gRPC server instance
-	proto.RegisterNodeServer(grpcServer, s) //registers the server implementation with gRPC
+func (n *Node) getClientInfo() *proto.Client {
+	n.LamportRequest = n.Lamport
+	return &proto.Client{
+		Id:           n.NodeId,
+		LamportClock: n.Lamport,
+	}
+}
 
-	listener, err := net.Listen("tcp", ":5050") //listens on TCP port using net.Listen
+func (s *NodeServer) start_server(port string) {
+	grpcServer := grpc.NewServer()                    //creates new gRPC server instance
+	proto.RegisterRicartArgawalaServer(grpcServer, s) //registers the server implementation with gRPC
+
+	listener, err := net.Listen("tcp", port) //listens on TCP port using net.Listen
 	if err != nil {
-		log.Fatalf("Did not work")
+		log.Fatalf("Did not work, failed listening on port %s", port)
 	}
 
 	if err != nil {
 		log.Fatalf("Did not work")
 	}
+
+	err = grpcServer.Serve(listener)
+	log.Printf("[Server] listening on port", port)
 }
 
 // utility method that on message receive checks max lamport and updates local
-func (s *NodeServer) updateLamportClockOnReceive(remoteLamport int32) int32 {
-	if remoteLamport > s.lamportClock {
-		s.lamportClock = remoteLamport
+func (s *Node) updateLamportClockOnReceive(remoteLamport int32) int32 {
+	if remoteLamport > s.Lamport {
+		s.Lamport = remoteLamport
 	}
-	s.lamportClock++
-	return s.lamportClock
+	s.Lamport++
+	return s.Lamport
 }
 
 // sets up logging both into a file and the terminal
@@ -68,4 +140,40 @@ func (s *NodeServer) setupLogging() {
 	s.logFile = logFile //saves the logfile to the client struct
 
 	log.SetOutput(io.MultiWriter(os.Stdout, logFile)) //sets the output to both the terminal and the file
+	log.SetFlags(log.Ldate | log.Ltime) //metadata written before each log message todo: include
 }
+
+/*
+	function for when a node recieves a request to enter the critical section from another node
+
+If this node has the state "HELD", it puts the other node in a queue
+If this node has the state "WANTED"
+*/
+func (s *Node) EnterRequest(ctx context.Context, in *proto.Client) (*proto.Reply, error) {
+
+	if s.State == "HELD" || s.State == "WANTED" && s.Lamport < in.LamportClock {
+		s.Queue = append(s.Queue, in)
+
+		//go routine til når den så selv har exittet
+		go s.WaitForReleased() //slet goroutine.
+		//return Empty
+	}
+
+	return &proto.Reply{Okay: "ok"}, nil
+}
+
+// go function, keeps looks at the state until it is set to released. Then returns, which "breaks" the go runction"
+func (s *Node) WaitForReleased() {
+	if s.State == "RELEASED" {
+		return
+	}
+}
+
+// sets the state to released, which then triggers the go routine WaitForReleased to end, such that the node will reply to all requests in the queue
+func (s *Node) Exit() {
+	s.State = "RELEASED"
+}
+
+//Make the queue: var requestQueue []string
+//Queue: requestQueue = append(requestQueue, request)
+//Dequeue: requestQueue = requestQueue[1:] //dequeue
